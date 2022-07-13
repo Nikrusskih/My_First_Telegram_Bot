7f5011feeca62dcee0f8dc4a34208b2e6de8919e
Nikolay Russkikh <nikrusskih@gmail.com>
2022-07-14 01:00:12 +0300

    First Bot

---
+++ src/test/java/io/projectdemo/NewJavaDemoBot/NewJavaDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projectdemo.NewJavaDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJavaDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
